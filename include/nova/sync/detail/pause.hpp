528d5ba5d23afb1c3e126eb2753c97f24aeaf982
Tim Blechmann <tim@klingt.org>
2026-04-28 13:37:53 +0800

    arm pause compile fixes

--- include/nova/sync/detail/pause.hpp
+++ include/nova/sync/detail/pause.hpp
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#if defined( __x86_64__ ) || defined( _M_X64 )
+#if defined( __x86_64__ ) || defined( _M_X64 ) || defined( __i386__ ) || defined( _M_IX86 )
 #  include <immintrin.h>
 #endif
 
@@ -15,14 +15,23 @@ namespace nova::sync::detail {
 
 inline void pause()
 {
-#if defined( __x86_64__ ) || defined( _M_X64 )
+#if defined( __x86_64__ ) || defined( _M_X64 ) || defined( __i386__ ) || defined( _M_IX86 )
     _mm_pause();
-#elif defined( __arm64__ ) || defined( __aarch64__ ) && defined( __GNUC__ )
-    __builtin_arm_isb( 0xF );
-#elif defined( _MSC_VER )
-    __isb( _ARM_BARRIER_SY );
+#elif defined( __arm64__ ) || defined( __aarch64__ ) || defined( __arm__ ) || defined( _M_ARM64 ) || defined( _M_ARM )
+#  if defined( _MSC_VER )
+#    if defined( _ARM_BARRIER_SY )
+#      define NOVA_SYNC__ARM_BARRIER_SY _ARM_BARRIER_SY
+#    else
+#      define NOVA_SYNC__ARM_BARRIER_SY 0xF
+#    endif
+    __isb( NOVA_SYNC__ARM_BARRIER_SY );
+#    undef NOVA_SYNC__ARM_BARRIER_SY
+#  else
+    __asm__ __volatile__( "isb" ::
+                              : "memory" );
+#  endif
 #else
-    ( (void)0 )
+    ( (void)0 );
 #endif
 }
 
