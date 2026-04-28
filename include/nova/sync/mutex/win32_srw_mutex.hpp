4c43039db226c189e958bf55e53644cfa00b2bc5
Tim Blechmann <tim@klingt.org>
2026-04-28 12:16:01 +0800

    mutex: remove over-reliance on hardware_destructive_interference_size

--- include/nova/sync/mutex/win32_srw_mutex.hpp
+++ include/nova/sync/mutex/win32_srw_mutex.hpp
@@ -17,7 +17,7 @@ namespace nova::sync {
 
 /// @brief Ultra-lightweight mutex using Win32 SRW lock.
 ///
-class alignas( detail::hardware_destructive_interference_size ) NOVA_SYNC_CAPABILITY( "mutex" ) win32_srw_mutex
+class NOVA_SYNC_CAPABILITY( "mutex" ) win32_srw_mutex
 {
 public:
     /// @brief Initialises the SRW lock in the unlocked state.
